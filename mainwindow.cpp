1d98955d61d0a08eb2438d3d8a725fd71196922e
kelychalitha <kcweerasooriya222@gmail.com>
2021-06-12 23:18:07 +0530

    Initial commit

---
+++ mainwindow.cpp
@@ -0,0 +1,49 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+
+    player = new QMediaPlayer(this);
+    vw = new QVideoWidget(this);
+    player->setVideoOutput(vw);
+    this->setCentralWidget(vw);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_actionOpen_triggered()
+{
+    QString filename = QFileDialog::getOpenFileName(this,"Open a File","","Video File(*.*)");
+    on_actionStop_triggered();
+
+    player->setMedia(QUrl::fromLocalFile(filename));
+
+    on_actionPlay_triggered();
+}
+
+void MainWindow::on_actionPlay_triggered()
+{
+    player->play();
+    ui->statusbar->showMessage("Playing");
+}
+
+void MainWindow::on_actionPause_triggered()
+{
+    player->pause();
+    ui->statusbar->showMessage("Paused...");
+}
+
+void MainWindow::on_actionStop_triggered()
+{
+    player->stop();
+    ui->statusbar->showMessage("Stopped");
+}
